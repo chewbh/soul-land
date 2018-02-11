87dae6f8d75b95719578111fa17917ea99e7f2ad
Chew Boon Heng <boonheng85@gmail.com>
2018-02-11 14:54:23 +0800

    added support for user and user group flipping strategies

--- soul-land-web/src/main/java/io/boonlogic/soul_land/web/SearchService.kt
+++ soul-land-web/src/main/java/io/boonlogic/soul_land/web/SearchService.kt
@@ -1,5 +1,7 @@
 package io.boonlogic.soul_land
 
+import io.boonlogic.util.feature.toggle.strategy.PARAM_GROUP_NAMES
+import io.boonlogic.util.feature.toggle.strategy.PARAM_USER_ID
 import org.ff4j.FF4j
 import org.ff4j.core.FlippingExecutionContext
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,8 +20,17 @@ class SearchService {
     fun search(@RequestParam("text") text: String): String {
 
         val fex = FlippingExecutionContext()
-        fex.addValue("user", "boonheng")
+        fex.addValue(PARAM_USER_ID, "boonheng")
         val newSearch = ff4j.check("newSearch", fex)
+        return if(newSearch) "New search is on: $text" else "legacy search: $text"
+    }
+
+    @GetMapping("/rest/testSearch")
+    fun testSearch(@RequestParam("text") text: String): String {
+
+        val fex = FlippingExecutionContext()
+        fex.addValue(PARAM_GROUP_NAMES, "boon, boonheng, hello, superman")
+        val newSearch = ff4j.check("testSearch", fex)
         return if(newSearch) "New search is on: $text" else "legacy search: $text"
     }
 
