a8007e8a7a74442fbea73c2be689b6fbe97547dd
Chew Boon Heng <boonheng85@gmail.com>
2018-02-15 22:15:38 +0800

    implemented custom feature store over RestTemplate

--- soul-land-web/src/main/java/io/boonlogic/soul/land/web/config/FeatureStoreRibbon.kt
+++ soul-land-web/src/main/java/io/boonlogic/soul/land/web/config/FeatureStoreRibbon.kt
@@ -2,9 +2,11 @@ package io.boonlogic.soul.land.web.config
 
 import org.ff4j.core.Feature
 import org.ff4j.exception.FeatureAccessException
+import org.ff4j.exception.FeatureAlreadyExistException
 import org.ff4j.exception.FeatureNotFoundException
 import org.ff4j.store.AbstractFeatureStore
 import org.ff4j.utils.json.FeatureJsonParser
+import org.ff4j.web.api.resources.domain.FeatureApiBean
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -27,9 +29,9 @@ class FeatureStoreRibbon(
 //    val url: String ??
 
     /** header parameter to add if secured mode enabled. */
-    val authorizationHeaderValue: String? by lazy {
+    val authorizationHeaderValue: String by lazy {
         if(apiKey.isNotEmpty()) buildAuthorization4ApiKey(apiKey)
-        else null
+        else ""
     }
 
     val storeRestUrl: String by lazy {
@@ -54,7 +56,7 @@ class FeatureStoreRibbon(
             throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
 
         val response = restTemplate.exchange("$storeRestUrl/$featureUid",
-            HttpMethod.GET, HttpEntity("parameters",headers()),
+            HttpMethod.GET, HttpEntity("parameters", headers()),
             String::class.java)
 
         if(response.statusCode.value() == 404)
@@ -68,39 +70,104 @@ class FeatureStoreRibbon(
     override fun enable(uid: String?) {
         if(uid == null || uid.length <= 0)
             throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
-//        OPERATION_ENABLE
-        restTemplate.postForEntity("$storeRestUrl/$OPERATION_ENABLE",)
 
-        val response = restTemplate.exchange("$storeRestUrl/$uid",
-            HttpMethod.POST, HttpEntity("parameters",headers()),
-            String::class.java)
+        val response =  restTemplate.postForEntity("$storeRestUrl/$uid/$OPERATION_ENABLE",
+            HttpEntity("", headers()), String::class.java)
 
+        if (response.statusCode.value() == 404) {
+            throw FeatureNotFoundException(uid)
+        }
     }
 
-    override fun clear() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun disable(uid: String?) {
+        if(uid == null || uid.length <= 0)
+            throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
+
+        val response =  restTemplate.postForEntity("$storeRestUrl/$uid/$OPERATION_DISABLE",
+            HttpEntity("", headers()), String::class.java)
+
+        if (response.statusCode.value() == 404) {
+            throw FeatureNotFoundException(uid)
+        }
+    }
+
+    override fun exist(uid: String?): Boolean {
+        if(uid == null || uid.length <= 0)
+            throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
+
+        val response =  restTemplate.exchange("$storeRestUrl/$uid",
+            HttpMethod.GET, HttpEntity("", headers()), String::class.java)
+
+        if(response.statusCode.is2xxSuccessful) return true
+        if(response.statusCode.value() == 404) return false
+        throw FeatureAccessException("Cannot check existence of feature, an HTTP error ${response.statusCodeValue} occured : ${response.statusCode.reasonPhrase}")
     }
 
     override fun create(fp: Feature?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        if(exist(fp?.uid ?: throw IllegalArgumentException("Feature cannot be null nor empty")))
+            throw FeatureAlreadyExistException(fp.uid)
 
-    override fun exist(featId: String?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        val response = restTemplate.exchange("$storeRestUrl/${fp.uid}",
+            HttpMethod.PUT, HttpEntity(FeatureApiBean(fp), headers()), String::class.java)
 
-    override fun update(fp: Feature?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-
-    override fun delete(fpId: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(!response.statusCode.is2xxSuccessful)
+            throw FeatureAccessException("Cannot create feature, an HTTP error ${response.statusCodeValue}$OCCURED")
     }
 
     override fun readAll(): MutableMap<String, Feature> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val response =  restTemplate.exchange(storeRestUrl,
+            HttpMethod.GET, HttpEntity("", headers()), String::class.java)
+        if(!response.statusCode.is2xxSuccessful)
+            throw FeatureAccessException("Cannot read features, an HTTP error ${response.statusCodeValue}$OCCURED")
+
+        val features = FeatureJsonParser.parseFeatureArray(response.body)
+        return features.associate { it.uid to it }.toMutableMap()
+    }
+
+    override fun delete(uid: String?) {
+        if(uid == null || uid.length <= 0)
+            throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
+
+        val response =  restTemplate.exchange("$storeRestUrl/$uid",
+            HttpMethod.DELETE, HttpEntity("", headers()), String::class.java)
+        if(response.statusCodeValue == 404) throw FeatureNotFoundException(uid)
+        if(response.statusCodeValue != 204)
+            throw FeatureAccessException("Cannot delete feature, an HTTP error ${response.statusCodeValue}$OCCURED")
+    }
+
+    override fun update(fp: Feature?) {
+        if(!exist(fp?.uid ?: throw IllegalArgumentException("Feature cannot be null nor empty")))
+            throw FeatureNotFoundException(fp.uid)
+
+        val response = restTemplate.exchange("$storeRestUrl/${fp.uid}",
+            HttpMethod.PUT, HttpEntity(FeatureApiBean(fp), headers()), String::class.java)
+
+        if(response.statusCodeValue != 204)
+            throw FeatureAccessException("Cannot update feature, an HTTP error ${response.statusCodeValue}$OCCURED")
+    }
+
+    override fun clear() {
+        if(serviceId.isEmpty())
+            throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
+
+        val response = restTemplate.exchange(
+            "http://$serviceId/$FF4J_API_URL/$FF4J_RESOURCE_STORE/$STORE_CLEAR",
+            HttpMethod.POST, HttpEntity("", headers()), String::class.java)
+
+        if(!response.statusCode.is2xxSuccessful)
+            throw FeatureAccessException("Cannot clear feature store - ${response.statusCode.reasonPhrase}")
+    }
+
+    override fun createSchema() {
+        if(serviceId.isEmpty())
+            throw IllegalArgumentException("[Assertion failed] - Parameter #0 (string)  must not be null nor empty")
+
+        val response = restTemplate.exchange(
+            "http://$serviceId/$FF4J_API_URL/$FF4J_RESOURCE_STORE/$STORE_CREATESCHEMA",
+            HttpMethod.POST, HttpEntity("", headers()), String::class.java)
+
+        if(!response.statusCode.is2xxSuccessful)
+            throw FeatureAccessException("Cannot create feature store - ${response.statusCode.reasonPhrase}")
     }
 }
 
@@ -122,6 +189,12 @@ const val FF4J_RESOURCE_STORE = "store"
 const val FF4J_RESOURCE_FEATURES = "features"
 
 const val FF4J_RESOURCE_GROUPS = "groups"
+
+/** relative path for cache. */
+const val STORE_CLEAR = "clear"
+
+/** relative path for cache. */
+const val STORE_CREATESCHEMA = "createSchema"
 
 /** Custom operation on resource.  */
 const val OPERATION_ENABLE = "enable"
