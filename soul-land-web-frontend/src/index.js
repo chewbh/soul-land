c6d3bf3b2abaad084f2bd3880450b9255d78b864
Chew Boon Heng <boonheng85@gmail.com>
2018-02-16 03:20:31 +0800

    added bootstrap

--- soul-land-web-frontend/src/index.js
+++ soul-land-web-frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap-theme.min.css'
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
